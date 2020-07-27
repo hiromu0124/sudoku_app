6251e9f992852cc4d22c25f7312f7c13e54b3298
hiromu0124 <totohiro0124@icloud.com>
2020-07-27 15:46:20 +0900

    データを保存機能追加

--- app/controllers/sudokues_controller.rb
+++ app/controllers/sudokues_controller.rb
@@ -3,9 +3,20 @@ class SudokuesController < ApplicationController
     
   end
   def new
-    
+    @sudoku=Sudoku.new
   end
   def create
-    @sudoku=Sudoku.new
+    @sudoku=Sudoku.create(sudoku_params)
+    # if @sudoku.save
+    #   redirect_to root_path, notice: "保存しました"
+    # else
+    #   render "new"
+    #   @sudoku=Sudoku.new
+    # end
+  end
+  
+  private
+  def sudoku_params
+    params.require(:sudoku).permit(:model_answer,:answer,:number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-root to: 'sudokues#index'
+root to: 'sudokues#new'
+resources :sudokues 
 end
