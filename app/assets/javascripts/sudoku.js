39d661475414faab8989d05e6c51a87fe900ffa6
hiromu0124 <totohiro0124@icloud.com>
2020-07-30 20:31:54 +0900

    form修正

--- app/assets/javascripts/sudoku.js
+++ app/assets/javascripts/sudoku.js
@@ -3,7 +3,7 @@ $(function(){
   function create_table(){
     let parent = $("#sudoku");
     var html = ""
-    var data = ""
+    
     for(var i = 0; i < 9; i++){
       if(i == 2 || i == 5){
         html += `<tr class="bottom_border">`
@@ -11,12 +11,16 @@ $(function(){
         html += `<tr class="sudoku_border">`
       }
       for(var k= 0; k < 9;k++){
+        for(var x = 1; x < 82; x++ ){
+          var data = x
+        }
         if(k == 2 || k == 5){
-          html += `<td class="right_border"><input value='${data}' maxlength="1" class="sudoku_form" name=""></td>`
+          html += `<td class="right_border"><input value='${data}' maxlength="1" class="sudoku_form" name="sudoku[model_answer]"></td>`
         }else{
           html += `<td><input  value='${data}' maxlength="1" class="sudoku_form" ></td>`
         }
       }
+      
       
       html += `</tr>`
     }
